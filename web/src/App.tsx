8d6c50494b97a67af573798bf5d1dfb6dc43a6b0
Nathan Hoyt <slimnatekc@gmail.com>
2026-03-11 00:08:12 -0500

    Create nav and footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,28 @@
 export default function App() {
-  return (
-    <main>
-      <h1>Skill Progress Builder</h1>
-      <p>React app served from web/dist.</p>
-    </main>
-  );
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li className="navbar-logo">
+                            <a href="/">skill-progress</a>
+                        </li>
+                        <li className="navbar-item">
+                            <a href="/builder">Builder</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>Skill Progress</h1>
+                <p>
+                    Skill Progress is a tool that allows you to create a
+                    progress bar for a skill.
+                </p>
+            </main>
+            <footer>
+                <p>Copyright 2026 Skill Progress</p>
+            </footer>
+        </>
+    );
 }
