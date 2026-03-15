f041bb76cffbc0cb12a55af64b44683b33c8c190
Nathan Hoyt <nathanstg@gmail.com>
2026-03-15 14:36:04 -0500

    Update dev command to run frontend live preview alongside server with tsx watch

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     root: path.resolve(__dirname),
     publicDir: path.resolve(__dirname, '../img'),
     plugins: [react()],
+    server: {
+        proxy: {
+            '/progress': 'http://localhost:3000',
+        },
+    },
     build: {
         outDir: path.resolve(__dirname, 'dist'),
         emptyOutDir: true,
